6e380501b3092c87d9ebf798cf47605cb0658e17
Christian Mürtz <chris.muertz@poolside.ai>
2026-02-26 14:14:07 +0100

    Fix model_dump_json and None issue

--- protobuf_to_pydantic/default_base_model.py
+++ protobuf_to_pydantic/default_base_model.py
@@ -42,7 +42,9 @@ class ProtobufCompatibleBaseModel(BaseModel):
                 continue
             field_value = output[field_name]
             output.pop(field_name)
-            output.update(field_value)
+
+            if field_value:
+                output.update(field_value)
         return output
 
     @field_serializer("*", when_used="json", mode="wrap")
@@ -121,7 +123,7 @@ class ProtobufCompatibleBaseModel(BaseModel):
         exclude: Optional[Any] = None,
         context: Optional[Dict[str, Any]] = None,
         by_alias: bool = False,
-        exclude_unset: bool = False, # True will break Trajectory serialization  
+        exclude_unset: bool = False, 
         exclude_defaults: bool = False,
         exclude_none: bool = False, 
         round_trip: bool = False,
@@ -151,9 +153,9 @@ class ProtobufCompatibleBaseModel(BaseModel):
         exclude: Optional[Any] = None,
         context: Optional[Dict[str, Any]] = None,
         by_alias: bool = False,
-        exclude_unset: bool = True,  # Changed default to True
-        exclude_defaults: bool = True,  # Changed default to True
-        exclude_none: bool = True,  # Changed default to True
+        exclude_unset: bool = False, 
+        exclude_defaults: bool = False,
+        exclude_none: bool = False, 
         round_trip: bool = False,
         warnings: bool = True,
         serialize_as_any: bool = False,
